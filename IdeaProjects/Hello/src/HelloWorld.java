0d29e5e88958e3884ae7c97e03c9547bf0277978
unknown <nicorescu.antonia@gmail.com>
2022-05-10 14:14:33 +0300

    C:\Users\Catalina\IdeaProjects\Hello

---
+++ IdeaProjects/Hello/src/HelloWorld.java
@@ -0,0 +1,25 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //public - access modifier
+
+        //sout
+        System.out.println("Hello, ALEX, Uratuleee! :P");
+
+        int myFirstNumber = (3 + 7) + (12/4); //  operators +-/*
+        int mySecondNumber = 12;
+        int myThridNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThridNumber;
+
+//        System.out.println(myFirstNumber + mySecondNumber + myThridNumber);
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println("My last one is " + myLastOne);
+
+
+
+
+    }
+
+}
