7f1d95f8802623201bdea32a69adc0f3c036258e
Antonia-Catalina Nicorescu <46438761+Antonia2019@users.noreply.github.com>
2022-06-16 18:31:37 +0300

    16.06.2022
    Data Structures

---
+++ IdeaProjects/DataStructures/src/ds/circularlinkedlist/Node.java
@@ -0,0 +1,11 @@
+package ds.circularlinkedlist;
+
+public class Node {
+
+    public int data;
+    public ds.singlylinklists.Node next;
+
+    public void displayNode(){
+        System.out.println("{ " + data + " }");
+    }
+}
